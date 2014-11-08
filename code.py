62dd9a6872935d582a656ffd965ca615069666d2
Øyvind Malin <oyvindmal@outlook.com>
2014-11-08 08:12:34 +0100

    Added output of sensordata

--- code.py
+++ code.py
@@ -1,13 +1,30 @@
 import web
+import tellcore.telldus as td
+import tellcore.constants as tdconst
 
 urls = (
 '/', 'index',
-'/test/(\d+)', 'test'
+'/test/(\d+)', 'test',
+'/telldus/sensor/(\d+)', 'telldusSensor'
 )
 
 class index:
 	def GET(self):
 		return "Hello World!"
+
+class telldusSensor:
+	def GET(self, id):
+		
+		core = td.TelldusCore()
+		sensor = ""
+		sensors = core.sensors()
+		for (i, s) in enumerate(sensors):
+			sensor_id = s.id
+			if sensor_id == int(id):
+				sensor = s
+
+		return str(sensor.temperature().value)
+		
 
 class test:
 	def GET(self, id):
